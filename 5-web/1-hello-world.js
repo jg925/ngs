b1b4b5d3ea0111d8a1c2fc6f2e1b31620680137d
jg925 <jg925@cornell.edu>
2022-02-24 16:23:28 -0500

    ngs done

--- 5-web/1-hello-world.js
+++ 5-web/1-hello-world.js
@@ -1,9 +1,16 @@
-const http = require('http');
+const http = require("http");
 
-const server = http.createServer((req, res) => {
-  res.end('Hello World\n');
-});
+const requestListener = (req, res) => {
+  // res.end("Hello World\n"); //end is the same as write + end
+  console.dir(res, { depth: 0 });
+  res.write("Hello World\n");
+  res.end(); // must end the session
+};
+
+// const server = http.createServer(requestListener); // this is an event emitter
+const server = http.createServer(); // this is an event emitter
+server.on("request", requestListener);
 
 server.listen(4242, () => {
-  console.log('Server is running...');
+  console.log("Server is running...");
 });
