8fef5a39afcd216984c771e01e81b4d777317cdc
jg925 <jg925@cornell.edu>
2022-02-23 21:21:11 -0500

    npm module

--- 2-modern-js/9-rest-spread.js
+++ 2-modern-js/9-rest-spread.js
@@ -1,11 +1,13 @@
 const [first, ...restOfItems] = [10, 20, 30, 40];
 
 const data = {
-  temp1: '001',
-  temp2: '002',
-  firstName: 'John',
-  lastName: 'Doe',
+  temp1: "001",
+  temp2: "002",
+  firstName: "John",
+  lastName: "Doe",
 };
+
+// these copies are shallow copies.
 
 const { temp1, temp2, ...person } = data;
 
